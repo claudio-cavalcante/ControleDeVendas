fce0f98b23abbe19b23d02d7e8a753de7a094375
viniciustads <vinicius_venancio2007@hotmail.com>
2017-05-14 11:53:02 -0300

    Método para retornar o map dos produtos

--- src/br/ufg/inf/produto/Estoque.java
+++ src/br/ufg/inf/produto/Estoque.java
@@ -1,6 +1,8 @@
 package br.ufg.inf.produto;
 
 import br.ufg.inf.pessoa.Funcionario;
+import br.ufg.inf.pessoa.Gerente;
+import kotlin.internal.InlineOnly;
 
 import java.util.AbstractMap;
 import java.util.HashMap;
@@ -17,20 +19,30 @@ public class Estoque {
 
     Map<Produto, Integer> produtosEmEstoque = new HashMap<Produto, Integer>();
 
-    public void adicionar(Funcionario funcionario,Produto produto,int qtd){
+    public void adicionar(Funcionario funcionario,Produto produto,int qtd) {
 
-        if(produtosEmEstoque.containsKey(produto)){
-            int qtdAnterior = produtosEmEstoque.get(produto);
-            produtosEmEstoque.replace(produto,produtosEmEstoque.get(produto),(produtosEmEstoque.get(produto)+qtd) );
+        if (funcionario instanceof Funcionario) {
+            System.out.println("Você não pode fazer isso manu");
+        } else {
+
+
+            if (produtosEmEstoque.containsKey(produto)) {
+                int qtdAnterior = produtosEmEstoque.get(produto);
+                produtosEmEstoque.replace(produto, produtosEmEstoque.get(produto), (produtosEmEstoque.get(produto) + qtd));
+            } else {
+                produtosEmEstoque.put(produto, qtd);
+            }
+
         }
-       else{
-            produtosEmEstoque.put(produto,qtd );
-        }
-
-
     }
 
-  
+    public Map<Produto, Integer> estoqueProdutos(){
+
+        return produtosEmEstoque;
+    }
+
+
+
     public void remover(Produto produto, int qtd){
 
         if(produtosEmEstoque.containsKey(produto)) {
