f8e2b88acc7192818582134d6aa4d1c2a087ae97
viniciustads <vinicius_venancio2007@hotmail.com>
2017-05-14 11:54:10 -0300

    Emissão do Estoque Total de produtos

--- src/br/ufg/inf/relatorio/RelatorioDeEstoque.java
+++ src/br/ufg/inf/relatorio/RelatorioDeEstoque.java
@@ -1,11 +1,41 @@
 package br.ufg.inf.relatorio;
 
+import br.ufg.inf.produto.Estoque;
+import br.ufg.inf.produto.Produto;
+
+import java.util.*;
+
 public class RelatorioDeEstoque implements Relatorio{
 
-	@Override
+	Estoque estoque = new Estoque();
+
+
+    public void estoqueAtual() {
+
+
+    }
+
+
+    public void estoqueDoDia(){
+
+    }
+
+
+    @Override
 	public String emitir() {
-		// TODO Auto-generated method stub
-		return null;
+		String relatorio = "";
+
+        Set<Map.Entry<Produto, Integer>> set = estoque.estoqueProdutos().entrySet();
+
+        for (Map.Entry<Produto, Integer> produto : set) {
+
+            relatorio += String.format("Código do produto: "+produto.getKey().getCodigo()+"\n" +
+                    "Descrição do Produto: "+produto.getKey().getDescricao()+"\n" +
+                    "Preço: "+produto.getKey().getPreco()+"\n"+
+                    "Quantidade: "+produto.getValue() );
+
+        }
+  		return relatorio;
 	}
 
 }
