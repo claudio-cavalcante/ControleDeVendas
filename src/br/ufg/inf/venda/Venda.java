e07865b1edad56ab89cd773a749d3dd8de8dd75e
Claudio Cavalcante Tonhá <claudio.cth@gmail.com>
2017-05-14 09:10:54 -0300

    Incluindo métodos classe venda conforme diagrama

--- src/br/ufg/inf/venda/Venda.java
+++ src/br/ufg/inf/venda/Venda.java
@@ -1,5 +1,39 @@
 package br.ufg.inf.venda;
 
-public class Venda {
+import java.util.List;
 
+import br.ufg.inf.pessoa.Funcionario;
+
+public class Venda {
+	private List<ItemVenda> itensVenda;
+	private Funcionario funcionario;
+	private FormaDePagamento formaDePagamento;
+	
+	public Venda(Funcionario funcionario, List<ItemVenda> itensVenda, FormaDePagamento formaDePagamento) {
+		super();
+		this.itensVenda = itensVenda;
+		this.funcionario = funcionario;
+		this.formaDePagamento = formaDePagamento;
+	}
+
+	public List<ItemVenda> getItensVenda() {
+		return itensVenda;
+	}
+
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	public FormaDePagamento getFormaDePagamento() {
+		return formaDePagamento;
+	}
+	
+	public double getValorTotal(){
+		double valorTotal = 0;
+		for(ItemVenda itemVenda : itensVenda){
+			valorTotal += itemVenda.getValorTotal();
+		}
+		
+		return valorTotal;
+	}
 }
