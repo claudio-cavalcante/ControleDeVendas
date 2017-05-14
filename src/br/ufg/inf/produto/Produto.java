2b3a2738baa33f5dc295e263ed32b90d9e43083f
Claudio Cavalcante Tonhá <claudio.cth@gmail.com>
2017-05-14 09:03:29 -0300

    classes produtos e item de venda.

--- src/br/ufg/inf/produto/Produto.java
+++ src/br/ufg/inf/produto/Produto.java
@@ -1,5 +1,21 @@
 package br.ufg.inf.produto;
 
 public class Produto {
+	private double preco;
+	private String descricao;
+	
+	public Produto(double preco, String descricao){
+		this.preco = preco;
+		this.descricao = descricao;
+	}
 
+	public double getPreco() {
+		return preco;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
 }
