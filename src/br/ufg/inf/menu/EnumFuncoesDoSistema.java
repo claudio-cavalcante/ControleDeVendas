4f7db00bf7d4b481785d5f6c99776ba71ad467c8
danillopneto <daynilou@gmail.com>
2017-05-17 21:31:50 -0300

    Ordenação de menus.

--- src/br/ufg/inf/menu/EnumFuncoesDoSistema.java
+++ src/br/ufg/inf/menu/EnumFuncoesDoSistema.java
@@ -1,14 +1,12 @@
 package br.ufg.inf.menu;
 
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import br.ufg.inf.pessoa.Funcionario;
 import br.ufg.inf.pessoa.Gerente;
 
 public enum EnumFuncoesDoSistema {
-	REALIZAR_VENDA, CONSULTAR_PRECO, ADICIONAR_PRODUTO_ESTOQUE, EMITIR_RELATORIO_VENDA, EMITIR_RELATORIO_ESTOQUE, LOGOFF, SAIR;
+	SAIR, LOGOFF, REALIZAR_VENDA, CONSULTAR_PRECO, ADICIONAR_PRODUTO_ESTOQUE, EMITIR_RELATORIO_VENDA, EMITIR_RELATORIO_ESTOQUE;
 
 	public static EnumFuncoesDoSistema getFuncao(int opcao) {
 		for (EnumFuncoesDoSistema funcao : EnumFuncoesDoSistema.values()) {
@@ -16,12 +14,14 @@ public enum EnumFuncoesDoSistema {
 				return funcao;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	public static Map<EnumFuncoesDoSistema, String> obtenhaFuncoes(Funcionario funcionario) {
-		Map<EnumFuncoesDoSistema, String> funcoes = new LinkedHashMap<EnumFuncoesDoSistema, String>();
+		Map<EnumFuncoesDoSistema, String> funcoes = new LinkedHashMap<EnumFuncoesDoSistema, String>();		
+		funcoes.put(SAIR, MensagensSistemaDeVendas.SAIR);
+		funcoes.put(LOGOFF, MensagensSistemaDeVendas.LOGOFF);
 		funcoes.put(REALIZAR_VENDA, MensagensSistemaDeVendas.REALIZAR_VENDA);
 		funcoes.put(CONSULTAR_PRECO, MensagensSistemaDeVendas.CONSULTAR_PRECO);
 		if (funcionario.getClass().getName() == Gerente.class.getName()) {
@@ -29,12 +29,10 @@ public enum EnumFuncoesDoSistema {
 			funcoes.put(EMITIR_RELATORIO_VENDA, MensagensSistemaDeVendas.EMITIR_RELATORIO_VENDA);
 			funcoes.put(EMITIR_RELATORIO_ESTOQUE, MensagensSistemaDeVendas.EMITIR_RELATORIO_ESTOQUE);
 		}
-		
-		funcoes.put(LOGOFF, MensagensSistemaDeVendas.LOGOFF);
-		funcoes.put(SAIR, MensagensSistemaDeVendas.SAIR);
+
 		return funcoes;
 	}
-	
+
 	public static boolean funcaoEhValida(String opcao) {
 		for (EnumFuncoesDoSistema funcao : EnumFuncoesDoSistema.values()) {
 			if (Integer.toString(funcao.ordinal()).equals(opcao.trim())) {
