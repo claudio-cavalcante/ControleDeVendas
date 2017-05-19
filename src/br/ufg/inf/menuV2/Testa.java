5a89c19af5c3d8bace5adbeaaf5c53a17efc08a7
danillopneto <daynilou@gmail.com>
2017-05-18 22:59:58 -0300

    Inclusão de menu para consultar preço no menu inicial.

--- src/br/ufg/inf/menuV2/Testa.java
+++ src/br/ufg/inf/menuV2/Testa.java
@@ -1,14 +1,35 @@
 package br.ufg.inf.menuV2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import br.ufg.inf.pessoa.Funcionario;
+import br.ufg.inf.pessoa.Gerente;
+import br.ufg.inf.produto.Estoque;
+import br.ufg.inf.produto.Produto;
+import br.ufg.inf.venda.Caixa;
 
 public class Testa {
-
-	public static void main(String[] args) {
-		Menu telaInicial = new Menu( new OpcaoMenuSair(),new OpcaoMenuLogin());
-		telaInicial.exibaOpcoes();
-		
+	private static List<Funcionario> funcionarios;
+	private static Funcionario funcionarioLogado;
+	private static List<Caixa> caixas;
+	private static Caixa caixaSelecionado;
 	
+	public static void main(String[] args) {
+		populeDados();
+		Menu telaInicial = new Menu(new OpcaoMenuSair(), new OpcaoMenuLogin(), new OpcaoMenuConsultarPreco());
+		telaInicial.exibaOpcoes();
 	}
 
+	private static void populeDados() {
+		funcionarios = new ArrayList<Funcionario>();
+		funcionarios.add(new Gerente(1, "Gerente"));
+		funcionarios.add(new Funcionario(2, "Cláudio"));
+		funcionarios.add(new Funcionario(3, "Danillo"));
+		funcionarios.add(new Funcionario(4, "Vinícius"));
+		caixas = new ArrayList<Caixa>();
+		Estoque.Instancia().adicionar(new Produto(1, "Leite", 5), 10);
+		Estoque.Instancia().adicionar(new Produto(2, "Ovos", 12), 10);
+		Estoque.Instancia().adicionar(new Produto(2, "Farinha", 2), 100);
+	}
 }
